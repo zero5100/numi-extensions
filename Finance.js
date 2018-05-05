236437a914e3ed05afed20c6df4182624f1f1cbe
Andy Kellerstrass <akellerstrass@gmail.com>
2018-05-05 04:41:29 -0500

    Add a Finance PV function

--- Finance.js
+++ Finance.js
@@ -52,3 +52,28 @@ function numiFV(values) {
 function findFutureValue(rate, nper, pv) {
   return (pv * Math.pow(1 + (rate / 100), nper));
 }
+
+// Similar to the Excel PV function.
+// Rate<percent> The interest rate per period.
+// Nper<integer> The total number of payment periods.
+// Fv<money> The future value of the investment.
+// RETURNS<money> The present value of an investment.
+numi.addFunction({
+  "id": "finance-pv", // Globally unique function name
+  "phrases": "pv" // Keyword used to call function
+}, numiPV);
+
+function numiPV(values) {
+  let rate = values[0].double;
+  let nper = values[1].double;
+  let fv = values[2].double;
+
+  return {
+    "double": findPresentValue(rate, nper, fv),
+    "unitId" : "USD"
+  };
+}
+
+function findPresentValue(rate, nper, fv) {
+  return (fv / Math.pow((1 + (rate / 100)), nper));
+}
